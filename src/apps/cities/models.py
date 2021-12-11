66596c113659b9941654b79204b99cf3af45c778
Brazzini OC <brazziniogosi@gmail.com>
2021-12-11 01:58:06 -0500

    add Cities & Stores apps

--- src/apps/cities/models.py
+++ src/apps/cities/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return "{} : {}".format(self.name, self.zip_code)
+
+
+
+
+class City(Location):
+    class Meta:
+        verbose_name = 'Ciudad'
+        verbose_name_plural = 'Ciudades'
+
+class State(Location):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'Estado'
+        verbose_name_plural = 'Estados'
+
+class Colony(Location):
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = 'Colonia'
+        verbose_name_plural = 'Colonias'
--- src/apps/stores/models.py
+++ src/apps/stores/models.py
@@ -1,29 +1,93 @@
 from django.db import models
 
 
-class Location(models.Model):
-    name = models.CharField(max_length=255)
-    zip_code = models.CharField(max_length=255)
 
-    class Meta:
-        abstract = True
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return "Nombre: {}, Código postal: {}".format(self.name, self.zip_code)
+        return self.name
 
-class City(Location):
     class Meta:
-        verbose_name = 'Ciudad'
-        verbose_name_plural = 'Ciudades'
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
 
-class State(Location):
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
-    class Meta:
-        verbose_name = 'Estado'
-        verbose_name_plural = 'Estados'
 
-class Colony(Location):
-    state = models.ForeignKey(State, on_delete=models.CASCADE)
+class Store(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='stores')
+    domain  = models.CharField(max_length=255, blank=True, null=True)
+    logo = models.ImageField(upload_to='stores/logos', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
     class Meta:
-        verbose_name = 'Colonia'
-        verbose_name_plural = 'Colonias'
+        verbose_name = 'Comercio'
+        verbose_name_plural = 'Comercios'
+
+
+class StoreLocation(models.Model):
+    address = models.TextField()
+    number = models.IntegerField()
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='locations')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} - N° {} X,Y: {} {}".format(self.address, self.number, self.longitude, self.latitude)
+
+    class Meta:
+        verbose_name = 'Ubicación de Comercio'
+        verbose_name_plural = 'Ubicacion de Comercios'
+
+
+class StoreLegalDetail(models.Model):
+    ruc = models.CharField(max_length=11, unique=True)
+    razon_social = models.CharField(max_length=150)
+    address = models.TextField()
+    store = models.OneToOneField(Store, on_delete=models.CASCADE, related_name='legal_detail')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.ruc, self.razon_social)
+
+    class Meta:
+        verbose_name = 'Detalle Legal de Comercio'
+        verbose_name_plural = 'Detalle Legal de Comercios'
+
+class StoreSubpage(models.Model):
+    about_us = models.TextField()
+    vission = models.TextField()
+    mission = models.TextField()
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='subpages')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Subpáginas de Comercio'
+        verbose_name_plural = 'Subpáginas de Comercios'
+
+
+class StoreSocialNetwork(models.Model):
+    facebook = models.CharField(max_length=255, blank=True, null=True)
+    instagram = models.CharField(max_length=255, blank=True, null=True)
+    tiktok = models.CharField(max_length=255, blank=True, null=True)
+    twitter = models.CharField(max_length=255, blank=True, null=True)
+    youtube = models.CharField(max_length=255, blank=True, null=True)
+    whatsapp = models.CharField(max_length=255, blank=True, null=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='social_networks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Redes Sociales de Comercio'
+        verbose_name_plural = 'Redes Sociales de Comercios'
