8175d747b0bbdeadb8a2ece6bd513f6832fd48b9
Brazzini OC <brazziniogosi@gmail.com>
2021-12-11 19:21:42 -0500

    Complete Cities app

--- src/apps/cities/admin.py
+++ src/apps/cities/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from apps.cities.models import City, State, Colony
 
-# Register your models here.
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = []
+    list_filter = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.list_display = [ 'name', 'zip_code', 'created_at', 'updated_at' ]
+        self.list_filter = [ 'name', 'zip_code', 'created_at', 'updated_at' ]
+
+
+class CityAdmin(LocationAdmin):
+    pass
+
+class StateAdmin(LocationAdmin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.list_display.append('city')
+        self.list_filter.append('city')
+
+class ColonyAdmin(LocationAdmin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.list_display.append('state')
+        self.list_filter.append('state')
+
+
+admin.site.register(City, CityAdmin)
+admin.site.register(State, StateAdmin)
+admin.site.register(Colony, ColonyAdmin)
+
--- src/apps/cities/models.py
+++ src/apps/cities/models.py
@@ -1,35 +1,40 @@
 from django.db import models
 
 
-
 class Location(models.Model):
-    name = models.CharField(max_length=255)
-    zip_code = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField('Nombre', max_length=255)
+    zip_code = models.CharField('Código Postal', max_length=255)
+    created_at = models.DateTimeField('Creado el', auto_now_add=True)
+    updated_at = models.DateTimeField('Actualizado el', auto_now=True)
 
     class Meta:
         abstract = True
 
     def __str__(self):
-        return "{} : {}".format(self.name, self.zip_code)
-
-
+        return self.name
 
 
 class City(Location):
+    pass
+
     class Meta:
         verbose_name = 'Ciudad'
         verbose_name_plural = 'Ciudades'
 
+
 class State(Location):
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE, related_name='state', verbose_name='Ciudad')
+
     class Meta:
         verbose_name = 'Estado'
         verbose_name_plural = 'Estados'
 
+
 class Colony(Location):
-    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    state = models.ForeignKey(
+        State, on_delete=models.CASCADE, related_name='colony', verbose_name='Estado')
+
     class Meta:
         verbose_name = 'Colonia'
         verbose_name_plural = 'Colonias'
